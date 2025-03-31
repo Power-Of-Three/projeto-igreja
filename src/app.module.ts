6f09ba7057f4e4b71f7a7a666c4f3e8783100911
josifermaodev <josianefermao@gmail.com>
2025-03-31 13:47:15 -0300

    :sparkles: feat: Cria a instalação de dependencias e configuração inicial

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      // useClass: ProdService,
+      // useClass: DevService,
+      imports: [ConfigModule],
+    }),
+    // AuthModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/data/services/dev.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@nestjs/common";
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from "@nestjs/typeorm";
+
+@Injectable()
+export class DevService implements TypeOrmOptionsFactory {
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'mysql',
+      host: process.env.DB_HOST || 'localhost',
+      port: process.env.MYSQL_PORT_INTERNAL
+        ? parseInt(process.env.MYSQL_PORT_INTERNAL)
+        : 3306,
+      username: process.env.DB_USERNAME || 'root',
+      password: process.env.DB_PASSWORD || 'root',
+      database: process.env.DB_NAME || 'db_fitness',
+      entities: [],
+      synchronize: true,
+    };
+  }
+}
